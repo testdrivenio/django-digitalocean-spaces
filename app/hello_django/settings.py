02570d26ba0882c6351a2aec37a7444c597f1bf1
Amir Tadrisi <amirtds@gmail.com>
2024-06-01 11:14:06 -0500

    chore: update libs and dependancies

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -135,3 +135,7 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:1337'
+]
